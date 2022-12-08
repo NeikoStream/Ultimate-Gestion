ba6122aa616c05e94f4ef6c8de2e5eba022efa09
NeikoStream <71373738+NeikoStream@users.noreply.github.com>
2022-12-08 20:59:38 +0100

    add margin 0 on all page + recenter button

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php require 'page/header.php'; ?>
+
+
+    <p>JE SUIS ICI</p>
+    <a href="/page/accueil">accueil</a>
+  </body>
+</html>
--- page/header.php
+++ page/header.php
@@ -1,17 +1,27 @@
-<html>
-    <!-- HEADER -->
-    <header>
-            
-        <!-- Logos -->
-        <div class="logo">
-            <a href="accueil.html"><img src="../img/frisbee.png" alt="Logo frisbee"></a>
-
-        </div>
-
-        <!-- menu de navigation -->
-        <nav> 
-
-        </nav>
-
-    </header>
-</html>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link href="./css/style.css" rel="stylesheet" />
+    <link rel="shortcut icon" type="image/png" href="./img/frisbee.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Connexion</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="logo">
+        <img src="./img/frisbee.png" alt="" width="130" />
+        
+      </div>
+      <div class="Texte-Logo">Toulouse Ultimate Club
+      </div>
+      <nav>
+        <ul>
+          <a href=""><li>Accueil</li></a>
+          <a href=""><li>Match</li></a>
+          <a href=""><li>Joueurs</li></a>
+          <a href=""><li>Statistique</li></a>
+        </ul>
+      </nav>
+    </div>
