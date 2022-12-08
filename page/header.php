3ddd4b0c7f1a118952889edd08fcf1e58b85bde9
Gaia Ducournau <gaia.ducournau@gmail.com>
2022-12-08 10:58:00 +0100

    update files

---
+++ page/header.php
@@ -0,0 +1,17 @@
+<html>
+    <!-- HEADER -->
+    <header>
+            
+        <!-- Logos -->
+        <div class="logo">
+            <a href="accueil.html"><img src="../img/frisbee.png" alt="Logo frisbee"></a>
+
+        </div>
+
+        <!-- menu de navigation -->
+        <nav> 
+
+        </nav>
+
+    </header>
+</html>
