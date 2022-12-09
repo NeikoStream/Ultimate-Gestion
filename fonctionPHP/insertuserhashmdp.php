91cc8df3182843adf5e37e6676e5156af83575ef
NeikoStream <71373738+NeikoStream@users.noreply.github.com>
2022-12-10 00:56:46 +0100

    Delete insertuserhashmdp.php

--- fonctionPHP/insertuserhashmdp.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'connexionbd.php';
-$passhash = password_hash('$iutinfo', PASSWORD_DEFAULT, ['cost'=>12]);
-
-$req = $linkpdo->prepare('UPDATE utilisateur SET mot_de_passe = :pass WHERE nom_utilisateur = :nvuser');
-$req -> execute(array('nvuser' => 'admin' , 'pass' => $passhash));
-
