f0785edc88a6573ca7ab2acb6901d97c27226c05
NeikoStream <71373738+NeikoStream@users.noreply.github.com>
2022-12-10 01:31:05 +0100

    match pdo update

--- page/match.php
+++ page/match.php
@@ -7,17 +7,19 @@ forcer_utilisateur_connecte();
 $title = "Matchs";
 require 'header.php'; 
 
+// METHODE avec PDO
 // récupérer tous les matchs
-// connexion à la base de données
- $db_username = 'u161682765_ultimatebd';
- $db_password = '7>vEV#s9t';
- $db_name = 'u161682765_ultimate';
- $db_host = '153.92.220.151:3306';
- $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
- or die('could not connect to database');
-//require '../fonctionPHP/verification.php';
-$query= mysqli_query($db,"SELECT datem,heurem,nom_equipe_adverse FROM matchs");
 
+require '../fonctionPHP/connexionbd.php';
+
+    ///Préparation de la requête sans les variables (marqueurs : nominatifs)
+
+    $requete = $linkpdo->prepare('SELECT datem,heurem,nom_equipe_adverse FROM matchs');
+ 
+    ///Liens entre variables PHP et marqueurs
+   $requete->execute();
+
+   
 ?>
     <h2>Liste des matchs</h2>
     <table>
@@ -29,16 +31,16 @@ $query= mysqli_query($db,"SELECT datem,heurem,nom_equipe_adverse FROM matchs");
             </tr>
         </thead>
         <tbody>
-            <?php while($row = mysqli_fetch_assoc($query)): ?>
+            <?php while($result = $requete->fetch()): ?>
             <tr>
-                <td><?php echo htmlspecialchars($row['datem']); ?></td>
-                <td><?php echo htmlspecialchars($row['heurem']); ?></td>
-                <td><?php echo htmlspecialchars($row['nom_equipe_adverse']); ?></td>
+                <td><?php echo htmlspecialchars($result['datem']); ?></td>
+                <td><?php echo htmlspecialchars($result['heurem']); ?></td>
+                <td><?php echo htmlspecialchars($result['nom_equipe_adverse']); ?></td>
             </tr>
             <?php endwhile; ?>
         </tbody>
     </table>
 
-    <button>Ajouter un match</button>*
+    <button>Ajouter un match</button>
 </body>
 </html>
