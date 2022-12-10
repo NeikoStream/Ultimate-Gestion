ba36fa090a992a1e0e0a724116a718b9f6ff7bed
NeikoStream <71373738+NeikoStream@users.noreply.github.com>
2022-12-10 23:00:42 +0100

    MENU FULL RESPONSIVE !!!

--- fonctionPHP/verification.php
+++ fonctionPHP/verification.php
@@ -1,45 +1,37 @@
 <?php
 session_start();
-if(isset($_POST['username']) && isset($_POST['password']))
-{
- 
- // connexion à la base de données AVEC PDO
- require 'connexionbd.php';
+if (isset($_POST['username']) && isset($_POST['password'])) {
 
- $username = $_POST['username'];
- $password = $_POST['password'];
-//AVEC PDO
-if($username !== "" && $password !== "")
- {
-    ///Préparation de la requête sans les variables (marqueurs : nominatifs)
+    // connexion à la base de données AVEC PDO
+    require 'connexionbd.php';
 
- $requete = $linkpdo->prepare('SELECT mot_de_passe FROM utilisateur where nom_utilisateur = :user');
- 
- ///Liens entre variables PHP et marqueurs
-$requete->execute([
-    'user'=> $username
-]);
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    //AVEC PDO
+    if ($username !== "" && $password !== "") {
+        ///Préparation de la requête sans les variables (marqueurs : nominatifs)
 
-$mdphash = $requete->fetch();
+        $requete = $linkpdo->prepare('SELECT mot_de_passe FROM utilisateur where nom_utilisateur = :user');
 
- if(password_verify($password,$mdphash[0])) // nom d'utilisateur et mot de passe correctes
- {
- $_SESSION['connecte'] = $username;
- header('Location: ../page/accueil.php');
- }
- else
- {
- header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
- }
- }
- else
- {
- header('Location: login.php?erreur=2'); // utilisateur ou mot de passe vide
- }
-}
-else
-{
- header('Location: login.php');
+        ///Liens entre variables PHP et marqueurs
+        $requete->execute([
+            'user' => $username
+        ]);
+
+        $mdphash = $requete->fetch();
+
+        if (password_verify($password, $mdphash[0])) // nom d'utilisateur et mot de passe correctes
+        {
+            $_SESSION['connecte'] = $username;
+            header('Location: ../page/accueil.php');
+        } else {
+            header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
+        }
+    } else {
+        header('Location: login.php?erreur=2'); // utilisateur ou mot de passe vide
+    }
+} else {
+    header('Location: login.php');
 }
 
 ?>
--- page/header.php
+++ page/header.php
@@ -1,29 +1,60 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link href="<?php if (isset($cheminStyle)) {echo $cheminStyle;} else { echo '../css/style.css';} ?>" rel="stylesheet" />
-    <link rel="shortcut icon" type="image/png" href="<?php if (isset($cheminLogo)) {echo $cheminLogo;} else { echo '../img/frisbee.png';} ?>" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?php if (isset($title)) {echo $title;} else { echo 'Ultimate';} ?></title>
-  </head>
-  <body>
-    <header class="container">
-      <div class="logo">
-        <img src="<?php if (isset($cheminLogo)) {echo $cheminLogo;} else { echo '../img/frisbee.png';} ?>" alt="" width="130" />
-        
-      </div>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <link href="../css/style.css" rel="stylesheet" />
+  <link rel="shortcut icon" type="image/png" href="../img/frisbee.png" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>
+    <?php if (isset($title)) {
+      echo $title;
+    } else {
+      echo 'Ultimate';
+    } ?>
+  </title>
+</head>
+
+<body>
+  <header class="container">
+    <nav class="navbar">
+    <div class="logo">
+      <img src="../img/frisbee.png" alt="" width="130" />
+
+    
       <h1 class="Texte-Logo">
-        Toulouse Ultimate Club
+      Toulouse Ultimate Club
       </h1>
-      <nav class="Menu-boutons">
-        <ul>
-          <a href="Accueil.php"><li>Accueil</li></a>
-          <a href="match.php"><li>Matchs</li></a>
-          <a href="saisieJoueur.php"><li>Joueurs</li></a>
-          <a href=""><li>Statistiques</li></a>
-          <a href="../fonctionPHP/logout.php"><li id="logout">Deconnexion</li></a>
-        </ul>
-      </nav>
-    </header>
+    </div>
+
+    <div class="nav-links">
+      <ul>
+        <li><a href="Accueil.php">
+            Accueil
+          </a></li>
+        <li><a href="match.php">
+            Matchs
+          </a></li>
+        <li><a href="saisieJoueur.php">
+            Joueurs
+          </a></li>
+        <li><a href="">
+            Statistiques
+          </a></li>
+        <li id="logout"><a href="../fonctionPHP/logout.php">
+            Deconnexion
+          </a></li>
+      </ul>
+      </div>
+      <img src="../img/menu-btn" alt="Menu hamburger" id="menuHamb">
+    </nav>
+  </header>
+  <script>
+        const menuHamburger = document.querySelector("#menuHamb")
+        const navLinks = document.querySelector(".nav-links")
+ 
+        menuHamburger.addEventListener('click',()=>{
+        navLinks.classList.toggle('mobile-menu')
+        })
+</script>
