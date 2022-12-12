96481bd515422e2d00bc10f73edcc3592bbf4c7b
Gaia Ducournau <119603647+gaiailou@users.noreply.github.com>
2022-12-12 11:10:22 +0100

    add joueurs.php

---
+++ page/joueurs.php
@@ -0,0 +1,36 @@
+<?php 
+
+//Partie empechant l'utilisateur non connecter a accéder au contenue (mettre en commentaire pour modifier le code facilement)
+require '../fonctionPHP/authentification.php';
+forcer_utilisateur_connecte();
+//Appel du header
+$title = "Joueurs";
+require 'header.php'; 
+
+require '../fonctionPHP/verification.php';
+require '../fonctionPHP/connexionbd.php';
+$query= $linkpdo->prepare('SELECT * FROM joueur');
+$query->execute();
+
+?>
+    <h2>Liste des joueurs</h2>
+    <li>
+        <?php while($row = $query->fetch()): ?>
+            <ul><a href="modifierJoueur.php">
+            <img class=photo_joueurs src=<?php echo htmlspecialchars($row['photo']); ?> alt="photo de <?php echo htmlspecialchars($row['prenom']); ?>
+            <?php echo htmlspecialchars($row['nom']); ?>" width="100">
+            <p><?php echo htmlspecialchars($row['prenom']); ?><?php echo htmlspecialchars($row['nom']); ?></p>
+            <p><?php echo htmlspecialchars($row['numero_licence']); ?></p>
+            <p><?php echo htmlspecialchars($row['date_naissance']); ?></p>
+            <p><?php echo htmlspecialchars($row['taille']); ?> cm</p>
+            <p><?php echo htmlspecialchars($row['poids']); ?> kg</p>
+            <p><?php echo htmlspecialchars($row['poste_prefere']); ?></p>
+            <p><?php echo htmlspecialchars($row['note_perso']); ?></p>
+            <p><?php echo htmlspecialchars($row['statut']); ?></p>
+        </a>
+        </ul>
+        <?php endwhile; ?>
+        <ul><a href="saisieJoueur.php">Ajouter un joueur</a></ul>
+    </li>
+</body>
+</html>
