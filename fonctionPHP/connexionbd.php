584a56c26a5ac3ad75b2da97e8f2bfc5738975b9
Nicolas Rousseau <71373738+NeikoStream@users.noreply.github.com>
2022-12-12 14:13:05 +0100

    connexion bd GitIgnore

--- fonctionPHP/connexionbd.php
+++ fonctionPHP/connexionbd.php
@@ -7,6 +7,6 @@ $db_host = '153.92.220.151:3306';
 try {
     $linkpdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_username, $db_password);
 } catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
+    die('Erreur : ' . $e->getMessage()); 
 }
 ?>
