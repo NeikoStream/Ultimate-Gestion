8b0de691004d3d5d8875ced938b103711c1a2728
NeikoStream <71373738+NeikoStream@users.noreply.github.com>
2022-12-19 15:07:55 +0100

    extract js

--- page/header.php
+++ page/header.php
@@ -50,11 +50,4 @@
       <img src="../img/menu-btn" alt="Menu hamburger" id="menuHamb">
     </nav>
   </header>
-  <script>
-        const menuHamburger = document.querySelector("#menuHamb")
-        const navLinks = document.querySelector(".nav-links")
- 
-        menuHamburger.addEventListener('click',()=>{
-        navLinks.classList.toggle('mobile-menu')
-        })
-</script>
+  <script src="../js/hamb.js"></script>
