d24beff87c3003e961a2a82e9dcf5aae448a6c5d
NeikoStream <71373738+NeikoStream@users.noreply.github.com>
2022-12-19 12:07:54 +0100

    add circle stat

--- page/statistiques.php
+++ page/statistiques.php
@@ -28,9 +28,68 @@ require '../fonctionPHP/connexionbd.php';
    $victoires = $victoires->fetch();
    $defaites = $defaites->fetch();
    $nuls = $nuls->fetch();
-   
+    $nbMatchs = $victoires[0]+$defaites[0]+$nuls[0]
 ?>
+<div class="winrate">
+    <div class="progress-circle" data-value="<?php echo (($victoires[0] + 0.5*$nuls[0]) / $nbMatchs * 100) ?>"></div>
+</div>
 
+<p>prc Victoire : <?php echo (($victoires[0] + 0.5*$nuls[0]) / $nbMatchs * 100) ?></p>
 <p>Victoire : <?php echo $victoires[0]?></p>
 <p>Défaites : <?php echo $defaites[0]?></p>
 <p>Nuls : <?php echo $nuls[0]?></p>
+
+
+
+
+
+
+
+<script>
+function createJauge(elem) {
+  if (elem) {
+    // on commence par un clear
+    while (elem.firstChild) {
+      elem.removeChild(elem.firstChild);
+    }
+    // création des éléments
+    var oMask  = document.createElement('DIV');
+    var oBarre = document.createElement('DIV');
+    var oSup50 = document.createElement('DIV');
+    // affectation des classes
+    oMask.className  = 'progress-masque';
+    oBarre.className = 'progress-barre';
+    oSup50.className = 'progress-sup50';
+    // construction de l'arbre
+    oMask.appendChild(oBarre);
+    oMask.appendChild(oSup50);
+    elem.appendChild(oMask);
+  }
+  return elem;
+}
+
+function initJauge(elem) {
+  var oBarre;
+  var angle;
+  var valeur;
+  //
+  createJauge( elem);
+  oBarre = elem.querySelector('.progress-barre');
+  valeur = elem.getAttribute('data-value');
+  valeur = valeur ? valeur * 1 : 0;
+  elem.setAttribute('data-value', valeur.toFixed(1));
+  angle = 360 * valeur / 100;
+  if (oBarre) {
+    oBarre.style.transform = 'rotate(' + angle + 'deg)';
+  }
+}
+
+// Initialisation après chargement du DOM
+document.addEventListener('DOMContentLoaded', function () {
+  var oJauges = document.querySelectorAll('.progress-circle');
+  var i, nb = oJauges.length;
+  for (i = 0; i < nb; i += 1) {
+    initJauge(oJauges[i]);
+  }
+});
+</script>
