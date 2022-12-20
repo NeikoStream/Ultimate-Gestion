16ddbec33be566caa6e61ca5a5c737364f5520db
NeikoStream <71373738+NeikoStream@users.noreply.github.com>
2022-12-20 01:20:28 +0100

    style of stat

--- page/statistiques.php
+++ page/statistiques.php
@@ -1,25 +1,25 @@
-<?php 
+<?php
 
 //Partie empechant l'utilisateur non connecter a accéder au contenue (metthe en commentaire pour modifier le code facilement)
 require '../fonctionPHP/authentification.php';
 forcer_utilisateur_connecte();
 //Appel du header
 $title = "Statistiques";
-require 'header.php'; 
+require 'header.php';
 
 // METHODE avec PDO
 // récupérer tous les matchs
 
 require '../fonctionPHP/connexionbd.php';
 
-    ///Préparation de la requête sans les variables (marqueurs : nominatifs)
+///Préparation de la requête sans les variables (marqueurs : nominatifs)
 
-    $victoires = $linkpdo->prepare('SELECT COUNT(*) FROM `matchs` WHERE score_equipe > score_adverse');
-    $defaites = $linkpdo->prepare('SELECT COUNT(*) FROM `matchs` WHERE score_equipe < score_adverse');
-    $nuls = $linkpdo->prepare('SELECT COUNT(*) FROM `matchs` WHERE score_equipe = score_adverse');
-    
-    //partie tableau joueurs
-    $joueurs = $linkpdo->prepare('SELECT CONCAT(prenom," ", nom) AS affichage_nom, poste_prefere , statut , 
+$victoires = $linkpdo->prepare('SELECT COUNT(*) FROM `matchs` WHERE score_equipe > score_adverse');
+$defaites = $linkpdo->prepare('SELECT COUNT(*) FROM `matchs` WHERE score_equipe < score_adverse');
+$nuls = $linkpdo->prepare('SELECT COUNT(*) FROM `matchs` WHERE score_equipe = score_adverse');
+
+//partie tableau joueurs
+$joueurs = $linkpdo->prepare('SELECT CONCAT(prenom," ", nom) AS affichage_nom, poste_prefere , statut , 
     count(case participer.etre_titulaire when 1 then 1 else null end) as titulaire,
     count(case participer.etre_titulaire when 0 then 1 else null end) as remplacant, 
     AVG(participer.performance) as moynotes,
@@ -33,32 +33,30 @@ require '../fonctionPHP/connexionbd.php';
     and participer.etre_titulaire = 1
     group by affichage_nom;');
 
-    $winjoueur = $linkpdo->prepare('SELECT COUNT(*) 
-    FROM joueur, matchs, participer 
-    WHERE joueur.numero_licence = :numlic 
-    and matchs.datem = participer.datem 
-    and matchs.heurem = participer.heurem 
-    and joueur.numero_licence = participer.numero_licence
-    and matchs.score_equipe>matchs.score_adverse;');
-    ///Liens enthe variables PHP et marqueurs
-   $victoires->execute();
-   $defaites->execute();
-   $nuls->execute();
-   $joueurs->execute();
-  
+///Liens enthe variables PHP et marqueurs
+$victoires->execute();
+$defaites->execute();
+$nuls->execute();
+$joueurs->execute();
 
-   ///Stockage des résultat
 
-   $victoires = $victoires->fetch();
-   $defaites = $defaites->fetch();
-   $nuls = $nuls->fetch();
-    $nbMatchs = $victoires[0]+$defaites[0]+$nuls[0]
-?>
+///Stockage des résultat
+
+$victoires = $victoires->fetch();
+$defaites = $defaites->fetch();
+$nuls = $nuls->fetch();
+$nbMatchs = $victoires[0] + $defaites[0] + $nuls[0]
+  ?>
 <div class="winrate">
-    <div class="progress-circle" data-value="<?php echo (($victoires[0] + 0.5*$nuls[0]) / $nbMatchs * 100) ?>"></div>
-
-<p>Victoire : <?php echo $victoires[0]?> Défaites : <?php echo $defaites[0]?> Nuls : <?php echo $nuls[0]?></p>
+  <h1>Taux de victoire</h1>
+  <div class="progress-circle" data-value="<?php echo (($victoires[0] + 0.5 * $nuls[0]) / $nbMatchs * 100) ?>"></div>
+  <p>
+  Match total : <?php echo $nbMatchs ?> <br>
+  Victoire : <?php echo $victoires[0] ?> <br>
+  Défaites : <?php echo $defaites[0] ?> <br>
+  Nuls : <?php echo $nuls[0] ?></p>
 </div>
+
 
 <table id="statjoueur">
   <thead id="colonne">
@@ -73,24 +71,24 @@ require '../fonctionPHP/connexionbd.php';
     </tr>
   </thead>
   <tbody id="donneeJoueur">
-  <?php 
-  while($result = $joueurs->fetch()): 
-  ?>
-            <tr>
-                <td><?php echo htmlspecialchars($result['affichage_nom']);?></td>
-                <td><?php echo htmlspecialchars($result['statut']); ?></td>
-                <td><?php echo htmlspecialchars($result['poste_prefere']); ?></td>
-                <td><?php echo htmlspecialchars($result['titulaire']); ?></td>
-                <td><?php echo htmlspecialchars($result['remplacant']); ?></td>
-                <td><?php echo htmlspecialchars($result['moynotes']); ?></td>
-                <td>
-                <?php 
-                  $winrate = (($result['win'] + 0.5*$result['draw'])/($result['win']+$result['loose']+$result['draw']) * 100);
-                  echo htmlspecialchars($winrate); 
-                  ?>
-                </td>
-            </tr>
-            <?php endwhile; ?>
+    <?php
+    while ($result = $joueurs->fetch()):
+    ?>
+    <tr>
+      <td><?php echo htmlspecialchars($result['affichage_nom']); ?></td>
+      <td><?php echo htmlspecialchars($result['statut']); ?></td>
+      <td><?php echo htmlspecialchars($result['poste_prefere']); ?></td>
+      <td><?php echo htmlspecialchars($result['titulaire']); ?></td>
+      <td><?php echo htmlspecialchars($result['remplacant']); ?></td>
+      <td><?php echo htmlspecialchars($result['moynotes']); ?></td>
+      <td>
+        <?php
+      $winrate = (($result['win'] + 0.5 * $result['draw']) / ($result['win'] + $result['loose'] + $result['draw']) * 100);
+      echo htmlspecialchars($winrate);
+        ?>
+      </td>
+    </tr>
+    <?php endwhile; ?>
   </tbody>
 </table>
 
