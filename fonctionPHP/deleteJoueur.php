54ba9dccf7318201d40201c4dbe96229d70af738
Nicolas Rousseau <71373738+NeikoStream@users.noreply.github.com>
2023-01-11 15:02:38 +0100

    deleteimg

--- fonctionPHP/deleteJoueur.php
+++ fonctionPHP/deleteJoueur.php
@@ -2,6 +2,11 @@
 require 'connexionbd.php';
 //appel a la method de recuperation du parmettre dans l'url
 $numlic = htmlspecialchars($_GET["lic"]);
+$imgJoueur = $linkpdo->prepare('SELECT photo from joueur where numero_licence = :numero_licence');
+$imgJoueur->execute(array('numero_licence' => $numlic));
+$imgj = $imgJoueur->fetch();
+
+unlink('../img/'.$imgj['photo']);
 $req = $linkpdo->prepare('DELETE FROM joueur WHERE numero_licence = :numero_licence');
 $req->execute(array('numero_licence' => $numlic));
 
