09a88aa2d778c191681496bd6ab283ad16a90693
NeikoStream <71373738+NeikoStream@users.noreply.github.com>
2023-01-24 23:17:32 +0100

    Footer add link

--- page/footer.php
+++ page/footer.php
@@ -1,5 +1,7 @@
         <footer>
-            <p>Ce site a été réalisé par Nicolas Rousseau et Gaïa Ducournau</p>
+            <div class="footerP">
+                <p>Ce <a href="https://github.com/NeikoStream/Ultimate-Gestion">site</a> a été réalisé par <a href="https://github.com/NeikoStream">Nicolas Rousseau</a> et <a href="https://github.com/gaiailou">Gaïa Ducournau</a>.</p>
+            </div>
         </footer>
     </body>
 </html>
